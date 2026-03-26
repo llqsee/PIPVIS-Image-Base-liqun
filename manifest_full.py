1a408db77c71d857eb6f8f66e1876eb591d41ec1
Piyush Mohan <dthq657@leeds.ac.uk>
2026-03-26 20:15:40 +0000

    [+] Updating Full-scale manifest

--- manifest_full.py
+++ manifest_full.py
@@ -3,7 +3,7 @@ import json
 
 # === CONFIGURATION ===
 # GitHub raw base path for Full-Scale images
-base_url = "https://raw.githubusercontent.com/piyushmohan01/PIPVIS-Image-Base/refs/heads/master/Full-Scale-Frames"
+base_url = "https://raw.githubusercontent.com/piyushmohan01/PIPVIS-Image-Base/master/Full-Scale-Frames"
 
 # Local directory where your Full-Scale frames are stored
 root = r"./Full-Scale-Frames"
